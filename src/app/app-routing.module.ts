3f95c5979bf758f1d4ad13222a258ef55eefff1e
strujilloor <strujillo1293@gmail.com>
2018-10-13 14:03:32 -0500

    agregando rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
+import {HeroesComponent} from './heroes/heroes.component';
 
+const routes: Routes = [
+    {path: 'heroes', component: HeroesComponent}
+];
 @NgModule({
-    exports: [RouterModule]
+    exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)]
 })
 export class AppRoutingModule {
 }
